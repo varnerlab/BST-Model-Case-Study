7e36bd0389b99451772a4abedc367cd7d9fa9435
Jeffrey Varner <jdv27@cornell.edu>
2022-10-14 11:15:58 -0400

    first commit

---
+++ Example-Code-Library.jl
@@ -0,0 +1,11 @@
+
+# setup paths -
+const _PROJECT_ROOT = pwd()
+const _PATH_TO_SRC = joinpath(_PROJECT_ROOT, "src")
+
+# load my codes -
+include(joinpath(_PATH_TO_SRC, "Utility.jl"))
+include(joinpath(_PATH_TO_SRC, "Factory.jl"))
+include(joinpath(_PATH_TO_SRC, "Kinetics.jl"))
+include(joinpath(_PATH_TO_SRC, "Balances.jl"))
+include(joinpath(_PATH_TO_SRC, "Simulation.jl"))
---
+++ src/Simulation.jl
@@ -0,0 +1,17 @@
+function evaluate(model::Dict{String,Any}; 
+    tspan::Tuple{Float64,Float64} = (0.0,20.0), Δt::Float64 = 0.01)
+
+    # get stuff from model -
+    xₒ = model["initial_condition_vector"]
+
+    # setup the solver -
+    prob = ODEProblem(balances, xₒ, tspan, model; saveat = Δt)
+    soln = solve(prob)
+
+    # get the results from the solver -
+    T = soln.t
+    X = soln.u
+
+    # return -
+    return (T,X)
+end
